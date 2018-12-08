b351e367232bffb816510f7f58c5146080406a7e
ph <ph@tidalgear.local>
2018-12-08 22:24:25 +0900

    improved

--- trigram.py
+++ trigram.py
@@ -2,26 +2,35 @@
 
 
 def encode(s):
-    t = ''
-    x = s.encode('utf-8') + b'\x00' * 4
-    for i in range(0, len(x) - 4, 3):
-        a, b, c = x[i + 0], x[i + 1], x[i + 2]
-        t += chr(0x2637 - (a & 7))
-        t += chr(0x2637 - ((a >> 3) & 7))
-        t += chr(0x2637 - ((a >> 6) | ((b & 1) << 2)))
-        t += chr(0x2637 - ((b >> 1) & 7))
-        t += chr(0x2637 - ((b >> 4) & 7))
-        t += chr(0x2637 - ((b >> 7) | ((c & 3) << 1)))
-        t += chr(0x2637 - ((c >> 2) & 7))
-        t += chr(0x2637 - ((c >> 5) & 7))
-    return t
+    if isinstance(s, str):
+        s = s.encode('utf-8')
+    r, t = '', None
+    for i, x in enumerate(s):
+        if i % 3 == 0:
+            r += chr(0x2637 - (x & 7))
+            r += chr(0x2637 - ((x >> 3) & 7))
+            t = x >> 6
+        elif i % 3 == 1:
+            r += chr(0x2637 - (t | ((x & 1) << 2)))
+            r += chr(0x2637 - ((x >> 1) & 7))
+            r += chr(0x2637 - ((x >> 4) & 7))
+            t = x >> 7
+        else:
+            r += chr(0x2637 - (t | ((x & 3) << 1)))
+            r += chr(0x2637 - ((x >> 2) & 7))
+            r += chr(0x2637 - ((x >> 5) & 7))
+            t = None
+    if t is not None:
+        r += chr(0x2637 - t)
+    return r
 
 
 def decode(s):
-    t = bytearray()
-    x = [0x2637 - ord(c) for c in s] + [0] * 8
-    for i in range(0, len(x) - 8, 8):
-        t.append(x[i + 0] | (x[i + 1] << 3) | ((x[i + 2] & 3) << 6))
-        t.append((x[i + 2] >> 2) | (x[i + 3] << 1) | (x[i + 4] << 4) | ((x[i + 5] & 1) << 7))
-        t.append((x[i + 5] >> 1) | (x[i + 6] << 2) | (x[i + 7] << 5))
-    return t.decode('utf8').rstrip('\x00')
+    r, t = b'', 0
+    for i, c in enumerate(s):
+        p = i & 7
+        t |= (0x2637 - ord(c)) << (p % 3 * 3 + p // 3)
+        if p in (2, 5, 7):
+            r += bytes([t & 0xff])
+            t >>= 8
+    return r.decode('utf-8')
